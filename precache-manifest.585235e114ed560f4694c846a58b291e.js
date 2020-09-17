d301aa2cb8610285c90ef48d083ba90142867886
Bharat Mane <bharat.mane@1985.co.in>
2020-09-17 08:12:01 +0530

    Updates

---
+++ precache-manifest.585235e114ed560f4694c846a58b291e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f7bcbc045fd4b8902748fcc75cedf2",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "9bb7bee6f01387ad6cbf",
+    "url": "/shopping-cart/static/css/main.a127641d.chunk.css"
+  },
+  {
+    "revision": "03942477b993ba925e39",
+    "url": "/shopping-cart/static/js/2.cb173fe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.cb173fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb7bee6f01387ad6cbf",
+    "url": "/shopping-cart/static/js/main.1bfcedca.chunk.js"
+  },
+  {
+    "revision": "06d78fa7905b535c708f",
+    "url": "/shopping-cart/static/js/runtime-main.d70d0bf8.js"
+  },
+  {
+    "revision": "d55c14bd74b90389796fd5442c7c12c3",
+    "url": "/shopping-cart/static/media/checkmark.d55c14bd.png"
+  }
+]);
